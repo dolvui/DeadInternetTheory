a5dc543ce365e7d4ccb9b30131a99484f33bbe34
ghidalia <noa.ghidalia@epita.fr>
2025-07-20 00:09:24 +0200

    post: start process for use veo3 + readme

--- ginnigen_veo3.py
+++ ginnigen_veo3.py
@@ -3,7 +3,7 @@ from gradio_client import Client
 def generate_with_veo(prompt):
 	client = Client("ginigen/VEO3-Free")
 	result = client.predict(
-			prompt="Inside a misty swamp at dawn, a humanoid alligator dressed in a pressed suit holds a vintage microphone with one scaly hand. Opposite him stands Lily, a startled frog wearing a straw hat and a raincoat. The alligator reporter clears his throat and asks, “Lily, can you lend me a lily pad?” Both freeze, then burst into croaky laughter, the frog clutching her belly, the alligator wiping tears of mirth. Sunbeams filter through Spanish moss, highlighting droplets on their suits.",
+			prompt=prompt,
 			nag_negative_prompt="Static, motionless, still, ugly, bad quality, worst quality, poorly drawn, low resolution, blurry, lack of details",
 			nag_scale=11,
 			height=480,
